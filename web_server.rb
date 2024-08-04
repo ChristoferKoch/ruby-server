caee6c7418bee99e0fd6716e10ff1f80cb1cd313
Christofer Koch <cbkoch13@gmail.com>
2024-08-04 14:40:55 -0700

    feat: add basic html file serving

---
+++ web_server.rb
@@ -0,0 +1,19 @@
+require 'socket'
+
+server_socket = TCPServer.new(12000)
+p server_socket
+
+server_socket.listen(1)
+
+while true
+  puts "Ready to serve..."
+  connection_socket = server_socket.accept
+  begin
+    message = connection_socket.recv(1024)
+    filename = message.split[1]
+    f = File.open('.' + filename)
+    output_data = f.read
+    connection_socket.puts "HTTP/1.1 200 OK\r\n\r\n" + output_data + "\r\n"
+  end
+  
+end

5cac6bfe5ae81337a49c352446fa67f612bac433
Christofer Koch <cbkoch13@gmail.com>
2024-08-04 14:49:06 -0700

    feat: add error handling

--- web_server.rb
+++ web_server.rb
@@ -12,6 +12,9 @@ while true
     message = connection_socket.recv(1024)
     filename = message.split[1]
     f = File.open('.' + filename)
+  rescue Errno::ENOENT
+    connection_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+  else
     output_data = f.read
     connection_socket.puts "HTTP/1.1 200 OK\r\n\r\n" + output_data + "\r\n"
   end
